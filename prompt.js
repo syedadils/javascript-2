c43619db29bc57694bc74bd885699f2d6c8469a9
Syed ADil <101401945+syedadils@users.noreply.github.com>
2022-05-21 05:45:47 -0700

    Add files via upload

---
+++ prompt.js
@@ -0,0 +1,18 @@
+// name alert with prompt
+var FullName =prompt("please Enter Your FullName");
+alert("Syed Adil");
+
+// city name prompt
+var cname = prompt("Enter your city Name");
+alert("Welcome to city of lights");
+
+// gander prpmpt
+var a = prompt("Enter Your Ganger");
+var b = ("male");
+var c = ("female");
+if (b){
+    alert("Good morning Sir");
+}
+  if(c){
+    alert("Good Morning Ma'am");
+}
